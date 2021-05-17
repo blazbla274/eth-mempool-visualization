6e4432c7f42f790ed184c182ad009da2d1e17e01
Blaze <blazej.blaut@itsilesia.com>
2021-05-17 20:45:56 +0200

    feat: Add app header and typography styles.

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import { Typography, TypographyTypeMap } from '@material-ui/core'
+import { OverridableComponent } from '@material-ui/core/OverridableComponent'
 import styled from 'styled-components'
 
 import { WAVES_PATHS, WAVES_TIMEOUT } from './constants'
@@ -9,9 +11,24 @@ const Container = styled.div`
   height: 200px;
 `
 
+const Svg = styled.svg`
+  position: absolute;
+  top: 0;
+  left: 0;
+`
+
 const Path = styled.path`
+  fill: ${({ theme }) => theme.palette.primary.main};
   transition: all ${WAVES_TIMEOUT}ms ease-in-out;
 `
+
+const AppTitle = styled(Typography)`
+  position: relative;
+
+  &.MuiTypography-root {
+    padding: ${({ theme }) => theme.spacing(2)}px;
+  }
+` as OverridableComponent<TypographyTypeMap<{}, "span">>
 
 const Navigation = () => {
   const [waveIndex, setWaveIndex] = useState<number>(0)
@@ -33,12 +50,13 @@ const Navigation = () => {
   return (
     <nav>
       <Container>
-        <svg
+        <Svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1440 320"
         >
           <Path fill="#0099ff" fill-opacity="1" d={WAVES_PATHS[waveIndex % WAVES_PATHS.length]}/>
-        </svg>
+        </Svg>
+        <AppTitle component="h1" variant="h3" >{process.env.REACT_APP_NAME}</AppTitle>
       </Container>
     </nav>
   )
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,15 +3,23 @@ import { plPL } from '@material-ui/core/locale'
 
 export const theme = createMuiTheme({
   palette: {
-    type: 'dark',
     primary: {
       main: '#2989f3',
       light: '#1da9ee',
       dark: '#0277cb'
     },
+    text: {
+      primary: 'white',
+      secondary: 'white',
+    },
     background: {
       default: '#333342',
       paper: '#3b3d4e'
+    },
+  },
+  typography: {
+    allVariants: {
+      color: 'white',
     }
   }
 }, plPL)
