08801d334de8a3a73eb58d59c800a85884e8fd13
Blaze <blazej.blaut@itsilesia.com>
2021-06-29 17:04:43 +0200

    refactor: Refactor types and add transaction circle animation.

--- src/components/BlockBuildingStage/BlockBuildingStage.tsx
+++ src/components/BlockBuildingStage/BlockBuildingStage.tsx
@@ -1,36 +1,31 @@
 import { useEffect } from 'react'
 import * as PIXI from 'pixi.js'
 
-import { usePendingTransactions } from 'hooks/pixiStage/usePendingTransactions'
 import { useRef } from 'react'
 import { useCurrentBlock } from 'hooks/useCurrentBlock'
 import { useWeb3Context } from 'providers/Web3Provider'
 import { Subscription } from 'types/web3'
-// import { pendingTransactionsSubscriptionFactory } from './pendingTransactions.utils'
-import { STAGE_PADDING } from 'constants/pixiStage'
 import { pendingTransactionsSubscriptionFactory } from './pendingTransactions.utils'
+
+export interface TransactionAnimationObject {
+  obj: any
+  initX: number
+  initY: number
+  speed: {
+    x: number
+    y: number
+  }
+}
+
+const transactions: TransactionAnimationObject[] = []
+
 
 const BlockBuildingStage = () => {
   const { web3Socket } = useWeb3Context()
   const appRef = useRef<PIXI.Application | null>(null)
   const block = useCurrentBlock()
-  // const s = usePendingTransactions(appRef)
-  // console.log('appRef', appRef.current)
 
   useEffect(() => {
-    const transactions: { obj: any, initX: number, initY: number }[] = []
-
-    const pushTr = (x: any) => {
-      transactions.push(x)
-    }
-
-    const gameLoop = () => {
-      transactions.forEach(({ obj, initX }) => {
-        obj.x = obj.x + initX > window.innerWidth / 2
-          ? obj.x - 0.5
-          : obj.x + 0.5
-      })
-    }
     if (appRef.current || !web3Socket) {
       return
     }
@@ -38,11 +33,59 @@ const BlockBuildingStage = () => {
     const app = new PIXI.Application({
       width: window.innerWidth,
       height: window.innerHeight / 2,
-      // transparent: true
+      transparent: true
     })
     appRef.current = app
     const anchor = document.getElementById('pixi-anchor')
     anchor?.appendChild(app.view)
+
+    const pushTransaction = (x: TransactionAnimationObject) => {
+      transactions.push(x)
+    }
+
+    const appCenterPoint = {
+      x: app.screen.width / 2,
+      y: app.screen.height / 2
+    }
+
+    const transactionBlockCircle  = new PIXI.Graphics()
+    transactionBlockCircle.beginFill(0x2976f2)
+    transactionBlockCircle.drawCircle(appCenterPoint.x, appCenterPoint.y, 20)
+    transactionBlockCircle.endFill()
+    transactionBlockCircle.zIndex = 22
+    app.stage.addChild(transactionBlockCircle)
+
+    const text = new PIXI.Text('0', {
+      fontSize: 24,
+      fill : 0xffffff,
+      align : 'center'
+    })
+    text.x = appCenterPoint.x - text.width / 2
+    text.y = appCenterPoint.y - text.height / 2
+
+    app.stage.addChild(text)
+
+    const gameLoop = (delta: number) => {
+      transactions.forEach(({ obj, initX, initY, speed }) => {
+        obj.x = obj.x + initX > appCenterPoint.x
+          ? obj.x - speed.x * delta
+          : obj.x + speed.x * delta
+
+        obj.y = obj.y + initY > appCenterPoint.y
+          ? obj.y - speed.y * delta
+          : obj.y + speed.y * delta
+      })
+
+      const transactionBlockCircleScale = 1 + (transactions.length / 100)
+      transactionBlockCircle.clear()
+      transactionBlockCircle.beginFill(0x2976f2)
+      transactionBlockCircle.drawCircle(appCenterPoint.x, appCenterPoint.y, 20 * transactionBlockCircleScale)
+      transactionBlockCircle.endFill()
+
+      text.text = transactions.length.toString()
+      text.x = appCenterPoint.x - text.width / 2
+      text.y = appCenterPoint.y - text.height / 2
+    }
 
     const listener = (event: any) => {
       app.view.style.width = `${window.innerWidth}px`
@@ -54,7 +97,7 @@ const BlockBuildingStage = () => {
     if (web3Socket) {
       web3SocketSubscription = web3Socket.eth.subscribe(
         'pendingTransactions',
-        pendingTransactionsSubscriptionFactory(appRef, pushTr)
+        pendingTransactionsSubscriptionFactory(appRef, pushTransaction)
       )
     }
     app.ticker.add(gameLoop)
@@ -84,7 +127,10 @@ const BlockBuildingStage = () => {
   useEffect(() => {
     const app = appRef.current
     if(app) {
-      app.stage.removeChildren()
+      transactions.forEach(child => {
+        app.stage.removeChild(child.obj)
+      })
+      transactions.splice(0, transactions.length)
     }
   }, [block])
 
--- src/components/BlockBuildingStage/pendingTransactions.utils.ts
+++ src/components/BlockBuildingStage/pendingTransactions.utils.ts
@@ -1,19 +1,34 @@
 import * as PIXI from 'pixi.js'
 import { STAGE_PADDING } from 'constants/pixiStage'
+import { TransactionAnimationObject } from './BlockBuildingStage'
 
-export const pendingTransactionsSubscriptionFactory = (app: any, pushTr: any) =>
+export const pendingTransactionsSubscriptionFactory = (
+  app: React.MutableRefObject<PIXI.Application | null>,
+  pushTransaction: (x: TransactionAnimationObject) => void
+) =>
   (error: Error, transactionHash: string) => {
-    if (!error) {
+    if (!error && app.current) {
       const gr  = new PIXI.Graphics()
       const x = Math.floor(Math.random() * (app.current.screen.width - 2 * STAGE_PADDING + 1)) + STAGE_PADDING
       const y = Math.floor(Math.random() * (app.current.screen.height - 2 * STAGE_PADDING + 1)) + STAGE_PADDING
 
       //@ts-ignore
-      gr.beginFill(0xffffff)
+      gr.beginFill(0x73b6ff)
       gr.drawCircle(x, y, STAGE_PADDING / 2)
       gr.endFill()
+      gr.zIndex = 1
+      app.current.stage.addChildAt(gr, 0)
 
-      app.current.stage.addChild(gr)
-      pushTr({ obj: gr, initX: x, initY: y })
+      const speed = {
+        x: Math.abs(window.innerWidth / 2 - x) / 100,
+        y: Math.abs(window.innerHeight / 2 / 2 - y) / 100
+      }
+
+      pushTransaction({
+        obj: gr,
+        initX: x,
+        initY: y,
+        speed
+      })
     }
   }
