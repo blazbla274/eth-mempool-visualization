0bb33b4c2453a0525a8c7131db46abab5bbbdb9a
Blaze <blazej.blaut@itsilesia.com>
2021-07-01 23:00:18 +0200

    feat: Add background.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import { AppProvider } from 'providers/AppProvider'
 import { Navigation } from 'components/Navigation/Navigation'
 import { BlockBuildingStage } from 'components/BlockBuildingStage/BlockBuildingStage'
 import { BannerHub } from 'components/BannersHub/BannerHub'
+import background from 'assets/background.png'
+
+const Container = styled.div`
+  background-image: url(${background});
+  background-size: cover;
+  min-height: 100vh;
+`
 
 const App = () => {
   return (
     <AppProvider>
-      <Navigation />
-      <BannerHub />
-      <BlockBuildingStage />
+      <Container>
+        <Navigation />
+        <BannerHub />
+        <BlockBuildingStage />
+      </Container>
     </AppProvider>
   )
 }
